95a846d769ec9e3c502fc2ff2702fbb888a8a205
Krzysztof Narloch <knarloch+GITHUB@gmail.com>
2024-03-05 08:28:45 +0100

    fix typo breaking build

--- stage2_1_fosiaudio/02-enable_overlayfs/01-run-chroot.sh
+++ stage2_1_fosiaudio/02-enable_overlayfs/01-run-chroot.sh
@@ -5,8 +5,8 @@ systemctl enable enable_overlayfs
 
 systemctl disable apt-daily-upgrade.timer
 systemctl disable apt-daily.timer
-systemctl disabledpkg-db-backup.timer
-systemctl disablee2scrub_all.timer
-systemctl disablefstrim.timer
-systemctl disablelogrotate.timer
-systemctl disableman-db.timer
+systemctl disable dpkg-db-backup.timer
+systemctl disable e2scrub_all.timer
+systemctl disable fstrim.timer
+systemctl disable logrotate.timer
+systemctl disable man-db.timer
