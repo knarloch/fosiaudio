c30f903df3ed5f0cf02e3ad2b9f143c90cfff2f7
Krzysztof Narloch <knarloch+GITHUB@gmail.com>
2024-03-05 08:23:13 +0100

    Disable daily timers. Limit log buffers
    Add changes preserving free disk space.
    Daily timers don't make much sense with overlayfs anyway.

--- stage2_1_fosiaudio/02-enable_overlayfs/00-run.sh
+++ stage2_1_fosiaudio/02-enable_overlayfs/00-run.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -ex
 
 install -m 755 files/enable_overlayfs	"${ROOTFS_DIR}/etc/init.d/"
+echo "SystemMaxUse=20M"                 "${ROOTFS_DIR}/etc/systemd/journald.conf"
+echo "SystemKeepFree=20M"               "${ROOTFS_DIR}/etc/systemd/journald.conf"
--- stage2_1_fosiaudio/02-enable_overlayfs/01-run-chroot.sh
+++ stage2_1_fosiaudio/02-enable_overlayfs/01-run-chroot.sh
@@ -1,4 +1,12 @@
 #!/bin/bash -ex
 
-echo "0 4 * * * /usr/sbin/reboot" >> /var/spool/cron/crontabs/root
+echo "0 5 * * * /usr/sbin/reboot" > /var/spool/cron/crontabs/root
 systemctl enable enable_overlayfs
+
+systemctl disable apt-daily-upgrade.timer
+systemctl disable apt-daily.timer
+systemctl disabledpkg-db-backup.timer
+systemctl disablee2scrub_all.timer
+systemctl disablefstrim.timer
+systemctl disablelogrotate.timer
+systemctl disableman-db.timer
