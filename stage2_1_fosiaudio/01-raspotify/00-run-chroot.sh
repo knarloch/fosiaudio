ffb830fca4348c8a7a40ba340f349b9ac8f97f63
Krzysztof Narloch <knarloch+GITHUB@gmail.com>
2026-02-24 07:52:48 +0100

    Fix raspotify package missing build error on clean build
    Before invoking raspotify install script, the package is not known to
    apt, so ignore failure at purging it.

--- stage2_1_fosiaudio/01-raspotify/00-run-chroot.sh
+++ stage2_1_fosiaudio/01-raspotify/00-run-chroot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
-sudo apt-get purge -y raspotify
+sudo apt-get purge -y raspotify ||true
 sudo curl -sL https://dtcooper.github.io/raspotify/install.sh | sh
 sudo cp "/etc/raspotify/conf" "/etc/raspotify/conf_default"
